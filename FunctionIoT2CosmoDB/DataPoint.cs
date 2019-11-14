f6411076929c6b689ab36673b4d288b8c4adba63
Marco Lewandowski <lewandowski@swms.de>
2019-11-14 16:23:47 +0100

    initial code for forwarding datappoints

---
+++ FunctionIoT2CosmoDB/DataPoint.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionIoT2CosmoDB
+{
+    class DataPoint
+    {
+            public ObjectId Id { get; set; }
+
+            public DateTime CreatedAtUtc { get; set; }
+
+            public Dictionary<string, double> Values { get; set; } = new Dictionary<string, double>();
+
+            public Dictionary<string, string> Tags { get; set; } = new Dictionary<string, string>();
+        }
+    
+}
--- FunctionIoT2CosmoDB/IoT2CosmosDb.cs
+++ FunctionIoT2CosmoDB/IoT2CosmosDb.cs
@@ -6,6 +6,12 @@ using Microsoft.Azure.EventHubs;
 using System.Text;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using System.Security.Authentication;
+using System;
+using MongoDB.Bson;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace FunctionIoT2CosmoDB
 {
@@ -16,7 +22,37 @@ namespace FunctionIoT2CosmoDB
         [FunctionName("Function1")]
         public static void Run([IoTHubTrigger("messages/events", Connection = "IoTConnectionString")]EventData message, ILogger log)
         {
-            log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
+            var mes = Encoding.UTF8.GetString(message.Body.Array);
+
+            log.LogInformation($"C# IoT Hub trigger function processed a message: {mes}");
+
+            var connectionString = System.Environment.GetEnvironmentVariable("MongoDbConnectionString", EnvironmentVariableTarget.Process);
+            var databaseString = System.Environment.GetEnvironmentVariable("MongoDbDatabaseString", EnvironmentVariableTarget.Process);
+            
+            MongoClientSettings settings = MongoClientSettings.FromUrl(
+                new MongoUrl(connectionString));
+
+            settings.SslSettings =
+              new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            settings.RetryWrites = false;
+            var mongoClient = new MongoClient(settings);
+
+            var client = new MongoClient(settings);
+            var database = client.GetDatabase(databaseString);
+
+            var collection = database.GetCollection<BsonDocument>((string)message.SystemProperties["iothub-connection-device-id"]);
+
+            mes = mes.Replace("true", "1");
+            mes = mes.Replace("false", "0");
+
+            Dictionary<string, double> values = JsonConvert.DeserializeObject<Dictionary<string, double>>(mes);
+
+            DateTime created = (DateTime)message.SystemProperties["iothub-enqueuedtime"];
+
+            var document = new DataPoint() { CreatedAtUtc = created, Values = values };
+
+            collection.InsertOne(document.ToBsonDocument());
+
         }
     }
 }
